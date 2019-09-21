5057288657d43c990934997564469b9697ca8a5d
RaviVaishnav20 <vaishnavravi1920@gmail.com>
2019-09-21 23:20:36 +0530

    Using a request module to get data from an API

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req, res){
   res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/", function(req, res){
+  var symbol = req.body.crypto + "" + req.body.fiat;
+
+  request("https://apiv2.bitcoinaverage.com/indices/global/ticker/"+symbol,function(error, response, body){
+    console.log(body);
+  });
 });
 
 app.listen(3000, function(){
